5c3d9919e29facc6873c9f10276dd6f5e3ee875d
iTito420 <aitorarias19@gmail.com>
2022-02-15 02:17:17 +0100

    Hubs Menu Decorated

--- src/main/java/online/nasgar/hubcore/hubcore/commands/menus/MenusCMD.java
+++ src/main/java/online/nasgar/hubcore/hubcore/commands/menus/MenusCMD.java
@@ -9,6 +9,7 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -149,7 +150,7 @@ public class  MenusCMD implements CommandExecutor {
             ItemStack dyeItem = new ItemStack(Material.INK_SACK, 1, (short) 1);
             ItemStack close = new ItemStack(dyeItem);
             ItemMeta closeMeta = close.getItemMeta();
-            closeMeta.setDisplayName(messageHandler.replacing(player, "MENUS.SERVERS.CLOSE"));
+            closeMeta.setDisplayName(messageHandler.replacing(player, "MENUS.CLOSE"));
             close.setItemMeta(closeMeta);
 
             inv.setItem(40, close);
@@ -158,7 +159,7 @@ public class  MenusCMD implements CommandExecutor {
             ItemStack frameBlack = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 15);
             for(int i : new int[]{0,1,2,3,4,5,6,7,8,9,17,18,26,27,35,36,37,38,39,41,42,43,44}) {
                     ItemMeta glassBlackMeta = frameBlack.getItemMeta();
-                    glassBlackMeta.setDisplayName(messageHandler.replacing(player, "MENUS.SERVERS.GLASSES"));
+                    glassBlackMeta.setDisplayName(messageHandler.replacing(player, "MENUS.GLASSES"));
                     frameBlack.setItemMeta(glassBlackMeta);
 
                     inv.setItem(i, frameBlack);
@@ -168,7 +169,73 @@ public class  MenusCMD implements CommandExecutor {
             ItemStack frameGray = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 7);
             for(int i : new int[]{10,11,12,13,14,15,16,19,20,22,24,25,28,29,30,31,32,33,34}) {
                 ItemMeta frameGrayMeta = frameGray.getItemMeta();
-                frameGrayMeta.setDisplayName(messageHandler.replacing(player, "MENUS.SERVERS.GLASSES"));
+                frameGrayMeta.setDisplayName(messageHandler.replacing(player, "MENUS.GLASSES"));
+                frameGray.setItemMeta(frameGrayMeta);
+
+
+                inv.setItem(i, frameGray);
+            }
+
+            player.openInventory(inv);
+
+            return true;
+        }
+        else if (args[0].equalsIgnoreCase("hubs")) {
+            Inventory inv = Bukkit.createInventory(player, 45, messageHandler.replacing(player, "MENUS.HUBS.TITLE"));
+
+            // HUB-1
+            ItemStack hub1 = new ItemStack(Material.GRASS);
+            ItemMeta hub1Meta = hub1.getItemMeta();
+            hub1Meta.setDisplayName(messageHandler.replacing(player, "MENUS.HUBS.ONE.NAME"));
+
+            String hub1Count = "%bungee_Hub-1%";
+            hub1Count = PlaceholderAPI.setPlaceholders(player, hub1Count);
+            int hub1CountInt = Integer.parseInt(hub1Count);
+
+            hub1Meta.setLore(messageHandler.replacingMany(player, "MENUS.HUBS.ONE.LORE", "%online%", hub1CountInt));
+            hub1.setItemMeta(hub1Meta);
+
+            inv.setItem(21, hub1);
+
+            // HUB-2
+            ItemStack hub2 = new ItemStack(Material.GRASS);
+            ItemMeta hub2meta = hub2.getItemMeta();
+            hub2meta.setDisplayName(messageHandler.replacing(player, "MENUS.HUBS.TWO.NAME"));
+
+            String hub2Count = "%bungee_Hub-2%";
+            hub2Count = PlaceholderAPI.setPlaceholders(player, hub2Count);
+            int hub2CountInt = Integer.parseInt(hub2Count);
+
+
+            hub2meta.setLore(messageHandler.replacingMany(player, "MENUS.HUBS.TWO.LORE", "%online%", hub2CountInt));
+            hub2.setItemMeta(hub2meta);
+
+            inv.setItem(23, hub2);
+
+            // CLOSE BUTTON
+            ItemStack dyeItem = new ItemStack(Material.INK_SACK, 1, (short) 1);
+            ItemStack close = new ItemStack(dyeItem);
+            ItemMeta closeMeta = close.getItemMeta();
+            closeMeta.setDisplayName(messageHandler.replacing(player, "MENUS.CLOSE"));
+            close.setItemMeta(closeMeta);
+
+            inv.setItem(40, close);
+
+            // FRAME STAINED GLASS BLACK
+            ItemStack frameBlack = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 15);
+            for(int i : new int[]{0,1,2,3,4,5,6,7,8,9,17,18,26,27,35,36,37,38,39,41,42,43,44}) {
+                ItemMeta glassBlackMeta = frameBlack.getItemMeta();
+                glassBlackMeta.setDisplayName(messageHandler.replacing(player, "MENUS.GLASSES"));
+                frameBlack.setItemMeta(glassBlackMeta);
+
+                inv.setItem(i, frameBlack);
+            }
+
+            // FRAME STAINED GLASS BLACK
+            ItemStack frameGray = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 7);
+            for(int i : new int[]{10,11,12,13,14,15,16,19,20,22,24,25,28,29,30,31,32,33,34}) {
+                ItemMeta frameGrayMeta = frameGray.getItemMeta();
+                frameGrayMeta.setDisplayName(messageHandler.replacing(player, "MENUS.GLASSES"));
                 frameGray.setItemMeta(frameGrayMeta);
 
 
