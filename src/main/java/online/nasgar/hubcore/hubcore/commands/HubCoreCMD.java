3ac40b9d28d4551feaaf8971e5b2385fc219d05b
iTito420 <aitorarias19@gmail.com>
2022-02-27 22:50:08 +0100

    edit: HubCoreCMD.java edited

--- src/main/java/online/nasgar/hubcore/hubcore/commands/HubCoreCMD.java
+++ src/main/java/online/nasgar/hubcore/hubcore/commands/HubCoreCMD.java
@@ -3,7 +3,6 @@ package online.nasgar.hubcore.hubcore.commands;
 import online.nasgar.hubcore.hubcore.HubCore;
 import online.nasgar.hubcore.hubcore.utils.LocationUtil;
 import online.nasgar.hubcore.hubcore.utils.Utils;
-import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -21,15 +20,26 @@ public class HubCoreCMD implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            plugin.reloadConfig();
-            Utils.log("&aReloaded completed");
+            if (args.length == 0) return false;
+            if (args[0].equalsIgnoreCase("reload")) {
+                plugin.reloadConfig();
+                Utils.log("&aReloaded completed");
+                return true;
+            }
+            if(args[0].equalsIgnoreCase("setspawn")) {
+                Utils.log("You must be a player to perform this command.");
+                return true;
+            }
             return false;
         }
+
         Player player = (Player) sender;
+
         if (!player.hasPermission("hubcore.reload")) {
             plugin.getMessageHandler().sendReplacing(sender, "NOPERMISSIONS.PREFIX", "%player%", sender.getName());
             return true;
         }
+
         if (args.length == 0) return false;
 
         if (args[0].equalsIgnoreCase("reload")) {
@@ -37,6 +47,12 @@ public class HubCoreCMD implements CommandExecutor {
             plugin.getMessageHandler().sendReplacing(sender, "CONFIGRELOAD.MESSAGE", "%player%", sender.getName());
             return true;
         }
+
+        if (!player.hasPermission("hubcore.setspawn")) {
+            plugin.getMessageHandler().sendReplacing(sender, "NOPERMISSIONS.PREFIX", "%player%", sender.getName());
+            return true;
+        }
+
         if(args[0].equalsIgnoreCase("setspawn")) {
             HubCore.getInstance().getConfig().set("LOCATION.SPAWN", LocationUtil.parseToString(player.getLocation()));
             HubCore.getInstance().saveConfig();
