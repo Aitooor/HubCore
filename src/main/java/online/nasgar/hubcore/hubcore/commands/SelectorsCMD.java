1bfb31b12bd0a9e2ff3e2d93244d5120a8a8ed20
iTito420 <aitorarias19@gmail.com>
2022-03-28 21:36:53 +0200

    edit: added lang

--- src/main/java/online/nasgar/hubcore/hubcore/commands/SelectorsCMD.java
+++ src/main/java/online/nasgar/hubcore/hubcore/commands/SelectorsCMD.java
@@ -55,7 +55,9 @@ public class SelectorsCMD implements CommandExecutor {
 
             int survivalCountInt = survival1Int + survival2Int;
 
-            survivalMeta.setLore(messageHandler.replacingMany(player, "MENUS.SERVERS.SURVIVAL.LORE", "%online%", survivalCountInt));
+            survivalMeta.setLore(messageHandler.replacingMany(player, "MENUS.SERVERS.SURVIVAL.LORE",
+                "%online%", survivalCountInt
+            ));
             survival.setItemMeta(survivalMeta);
 
             inv.setItem(21, survival);
