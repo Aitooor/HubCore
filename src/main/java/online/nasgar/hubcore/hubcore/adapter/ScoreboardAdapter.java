73227a05c9fe2331862060e2d1ef7859e1ce56e9
iTito420 <aitorarias19@gmail.com>
2022-12-29 18:56:33 +0100

    fix: improve code of scoreboard

--- src/main/java/online/nasgar/hubcore/hubcore/adapter/ScoreboardAdapter.java
+++ src/main/java/online/nasgar/hubcore/hubcore/adapter/ScoreboardAdapter.java
@@ -1,6 +1,7 @@
 package online.nasgar.hubcore.hubcore.adapter;
 
 import me.clip.placeholderapi.PlaceholderAPI;
+import me.yushust.message.MessageHandler;
 import online.nasgar.hubcore.hubcore.HubCore;
 import online.nasgar.hubcore.hubcore.utils.Utils;
 import online.nasgar.hubcore.hubcore.managers.scoreboard.AssembleAdapter;
@@ -12,7 +13,8 @@ import java.util.List;
 
 public class ScoreboardAdapter implements AssembleAdapter {
 
-    FileConfiguration config = HubCore.getInstance().getConfig();
+    private FileConfiguration config = HubCore.getInstance().getConfig();
+    private MessageHandler messageHandler = HubCore.getInstance().getMessageHandler();
 
     @Override
     public String getTitle(Player player) {
@@ -21,18 +23,13 @@ public class ScoreboardAdapter implements AssembleAdapter {
 
     @Override
     public List<String> getLines(Player player) {
-        String playerName = "%player_name%";
-        String online = "%server_online%";
-        String bungee_online = "%bungee_total%";
+        String playerName = player.getDisplayName();
         String vaultPrefix = "%vault_prefix%";
-        String localTime = "%localtime_time_yyyy/MM/dd%";
 
         return Utils.translate(PlaceholderAPI.setPlaceholders(
                 player.getPlayer(),
-                HubCore.getInstance().getMessageHandler().replacingMany(
+                messageHandler.replacingMany(
                         player, "SCOREBOARD.LINES",
-                        "%online%", online,
-                        "%bungee_online%", bungee_online,
                         "%player%", playerName,
                         "%rank%",PlaceholderAPI.setPlaceholders(player, vaultPrefix))));
     }
